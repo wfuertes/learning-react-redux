d826722e06b4d9d2c6102fcd5574c7a1b4bfba1c
Willian F. Batista <wfuertes@gmail.com>
2016-11-03 14:15:18 -0200

    Initial commit: dev env

---
+++ src/index.test.js
@@ -0,0 +1,15 @@
+import expect from 'expect';
+
+describe('Our first test', () => {
+    it('should pass', () => {
+        expect(true).toEqual(true);
+    });
+    
+    it('should pass', () => {
+        expect(true).toEqual(true);
+    });
+
+    it('should pass', () => {
+        expect(true).toEqual(true);
+    });
+});
